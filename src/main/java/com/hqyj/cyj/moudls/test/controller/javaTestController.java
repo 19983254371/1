010cf0e778895ec8e892ea19e82899229c3f1c33
樱冥神舞 <1142101407@qq.com>
2020-08-11 13:26:53 +0800

    配置文件

--- src/main/java/com/hqyj/cyj/moudls/test/controller/javaTestController.java
+++ src/main/java/com/hqyj/cyj/moudls/test/controller/javaTestController.java
@@ -1,20 +1,45 @@
 package com.hqyj.cyj.moudls.test.controller;
 
 
+import com.hqyj.cyj.moudls.test.vo.Test2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @Controller
 @RequestMapping("/test")
 public class javaTestController {
 
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(javaTestController.class);
+
     @GetMapping("/firstTest")
     @ResponseBody
     public String test(){
         return "this is test of my first springboot";
+    }
+
+    @Autowired
+    private Test2 test;
+
+    @GetMapping("test2")
+    @ResponseBody
+    public void test2(){
+
+    }
+    @GetMapping("/logTest")
+    @ResponseBody
+    public String logTest() {
+        LOGGER.trace("This is trace log");
+        LOGGER.debug("This is debug log");
+        LOGGER.info("This is info log");
+        LOGGER.warn("This is warn log");
+        LOGGER.error("This is error log");
+        return "This is log test";
     }
 }
